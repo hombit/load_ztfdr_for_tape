ba2fc48a945be18dd2f2d06d230ff216ed31e888
Konstantin Malanchev <hombit@gmail.com>
2023-12-12 16:44:36 -0500

    Move filterid from objects to sources

--- src/load_ztfdr_for_tape/pandas.py
+++ src/load_ztfdr_for_tape/pandas.py
@@ -5,7 +5,7 @@ import pandas as pd
 import polars as pl
 
 from load_ztfdr_for_tape.columns import (ID_COLUMN, OBJECT_COLUMNS,
-                                         SOURCE_COLUMNS)
+                                         SOURCE_COLUMNS, TIME_DOMAIN_COLUMNS)
 
 __all__ = ["load_object_df", "load_source_df"]
 
@@ -34,7 +34,11 @@ def load_object_df(path: Union[str, Path], columns: Iterable[str] = OBJECT_COLUM
     return pandas_df
 
 
-def load_source_df(path: Union[str, Path], columns: Iterable[str] = SOURCE_COLUMNS) -> pd.DataFrame:
+def load_source_df(
+        path: Union[str, Path],
+        time_domain_columns: Iterable[str] = TIME_DOMAIN_COLUMNS,
+        source_columns: Iterable[str] = SOURCE_COLUMNS
+) -> pd.DataFrame:
     """Load the "source" dataframe from a ZTF DR datafile.
 
     It loads objectid column and columns representing light curves.
@@ -44,19 +48,21 @@ def load_source_df(path: Union[str, Path], columns: Iterable[str] = SOURCE_COLUM
     ----------
     path : str or Path
         Path to the datafile to load.
-    columns : iterable of str
-        Columns to load from the datafile. By default, it loads objectid
-        column and columns representing light curves.
+    time_domain_columns : iterable of str
+        Columns with time-domain nested array data. By default, it loads all
+        the columns that represent light curves.
+    source_columns : iterable of str
+        Columns to load from the datafile. By default, it loads objectid,
+        filterid and all the columns that represent light curves. It must
+        be a superset of `time_domain_columns`.
 
     Returns
     -------
     pd.DataFrame
         A pandas dataframe with the source table.
     """
-    columns = list(columns)
-
-    polars_nested_df = pl.read_parquet(path, columns=[ID_COLUMN] + columns)
-    polars_flat_df = polars_nested_df.explode(columns)
+    polars_nested_df = pl.read_parquet(path, columns=[ID_COLUMN] + list(source_columns))
+    polars_flat_df = polars_nested_df.explode(*time_domain_columns)
     pandas_df = polars_flat_df.to_pandas(use_pyarrow_extension_array=True)
     pandas_df.set_index(ID_COLUMN, inplace=True)
     return pandas_df
